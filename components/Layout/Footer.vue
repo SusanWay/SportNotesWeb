f61a7719d185b510ac7a93ee050cdb1d0c0ce03c
SusanWay <zaev_2030@mail.ru>
2023-11-30 15:26:04 +0300

    refactor footer

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -3,24 +3,15 @@
 </script>
 
 <template>
-  <footer class="sticky bottom-0 z-10 flex w-full items-center justify-between bg-gray p-7">
+  <footer class="sticky bottom-0 z-10 flex w-full justify-around gap-5 bg-gray p-7 ">
     <NuxtLink to="/#">
-      <Icon name="Menu/HomeAlt" size="40" class="text-white" />
+      <Icon name="Menu/DumbbellLarge" size="40" class="text-white" />
     </NuxtLink>
     <NuxtLink to="/#">
       <Icon name="Menu/HomeAlt" size="40" class="text-white" />
     </NuxtLink>
     <NuxtLink to="/#">
-      <div class="relative h-16 w-16 rounded-full bg-lime">
-        <span class="absolute left-1/2 top-1/2 h-1 w-8 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-black" />
-        <span class="absolute left-1/2 top-1/2 h-8 w-1 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-black" />
-      </div>
-    </NuxtLink>
-    <NuxtLink to="/#">
-      <Icon name="Menu/HomeAlt" size="40" class="text-white" />
-    </NuxtLink>
-    <NuxtLink to="/#">
-      <Icon name="Menu/HomeAlt" size="40" class="text-white" />
+      <Icon name="Menu/FoodOutLine" size="40" class="text-white" />
     </NuxtLink>
   </footer>
 </template>
