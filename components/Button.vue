5d7f88cad7dccc62e78c815fe503b1e393bf9391
SusanWay <zaev_2030@mail.ru>
2023-12-04 11:49:16 +0300

    update workoutAdd modal

--- components/Button.vue
+++ components/Button.vue
@@ -18,7 +18,7 @@ const component = computed(() => {
     :is="component"
     :to="to"
     :disabled="disabled"
-    class="relative inline-flex h-14 items-center justify-center gap-2.5 rounded-2xl bg-lime px-7 font-title text-base font-extrabold uppercase text-white transition-colors duration-300 hover:bg-lime-light hover:text-black"
+    class="inline-flex h-14 items-center justify-center gap-2.5 rounded-2xl bg-lime px-7 font-title text-base font-extrabold uppercase text-white transition-colors duration-300 hover:bg-lime-light hover:text-black"
     :class="{ 'w-full': block }"
   >
     <span :class="{ 'opacity-0': loading }">
--- components/Modals/AddWorkout.vue
+++ components/Modals/AddWorkout.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-import { VueFinalModal } from 'vue-final-modal'
+import { VueFinalModal, useVfm } from 'vue-final-modal'
 
-const selectContentList = [
-  'Тренировка',
-  'Бег',
-]
+const vfm = useVfm()
 </script>
 
 <template>
   <VueFinalModal
     modal-id="addWorkout"
-    class="fixed inset-0 z-20 bg-gray-dark px-5 py-7"
+    class="inset-0 flex flex-col justify-between bg-gray-dark px-5 py-7"
     :hide-overlay="true"
   >
-    <div>
-      <InputTitle text="Введите название тренировки" />
+    <button type="button" class="absolute left-6 top-6 z-10">
+      <Icon name="Nav/Save" size="32" class="text-lime-light" />
+    </button>
+    <button type="button" class="absolute right-6 top-6 z-10" @click="vfm.close('addWorkout')">
+      <div class="relative flex h-8 w-8">
+        <span class="absolute top-1/3 h-0.5 w-full translate-y-1 rotate-45 bg-white" />
+        <span class="absolute bottom-1/3 h-0.5 w-full -translate-y-1 -rotate-45 bg-white" />
+      </div>
+    </button>
+    <div class="mt-2.5">
+      <InputTitle text="Название тренировки" />
     </div>
   </VueFinalModal>
 </template>
